b0f1155cbd98acdddfdb3b1db05a96bcb7a0d2c7
Shubham Singh <shubhamrajput2702@gmail.com>
2025-11-25 11:09:41 +0530

    Add portfolio content data and project information

---
+++ src/data/mockData.ts
@@ -0,0 +1,10 @@
+export const userProfile = {
+  name: 'Shubham Singh',
+  tagline: 'Crafting beautiful and functional web experiences.',
+  resumeUrl: '/resume_tex.pdf',
+  socials: {
+    github: 'https://github.com/shubhamrajput27',
+    linkedin: 'https://www.linkedin.com/in/shubham-singh-a96623290/',
+    instagram: 'https://www.instagram.com/shubh_rajput.27?igsh=MTlwMTR4Z3k0MjVqag=='
+  }
+};
